8b894b52f30985cac49f15b31ef52adbc1c12937
gba <gba@brute.undef.net>
2026-08-15 08:30:56 -0700

    Allow deterministic SSH config in HIL runs

--- scripts/aryaos-test/run.sh
+++ scripts/aryaos-test/run.sh
@@ -33,22 +33,26 @@ if [[ -n "${ARYAOS_SSH_KNOWN_HOSTS_FILE:-}" ]]; then
 else
 	KNOWN_HOSTS_ARGS=(-o StrictHostKeyChecking=accept-new)
 fi
+SSH_CONFIG_ARGS=()
+if [[ -n "${ARYAOS_SSH_CONFIG_FILE:-}" ]]; then
+	SSH_CONFIG_ARGS=(-F "${ARYAOS_SSH_CONFIG_FILE}")
+fi
 
-SSH=(ssh -o BatchMode=yes -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
-SCP=(scp "${KNOWN_HOSTS_ARGS[@]}")
+SSH=(ssh "${SSH_CONFIG_ARGS[@]}" -o BatchMode=yes -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
+SCP=(scp "${SSH_CONFIG_ARGS[@]}" "${KNOWN_HOSTS_ARGS[@]}")
 SSH_METHOD="ssh defaults"
 
 if [[ -n "${ARYAOS_DEV_PI_SSH_KEY:-}" && -r "${ARYAOS_DEV_PI_SSH_KEY}" ]]; then
-	SSH=(ssh -i "${ARYAOS_DEV_PI_SSH_KEY}" -o IdentitiesOnly=yes -o BatchMode=yes -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
-	SCP=(scp -i "${ARYAOS_DEV_PI_SSH_KEY}" -o IdentitiesOnly=yes "${KNOWN_HOSTS_ARGS[@]}")
+	SSH=(ssh "${SSH_CONFIG_ARGS[@]}" -i "${ARYAOS_DEV_PI_SSH_KEY}" -o IdentitiesOnly=yes -o BatchMode=yes -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
+	SCP=(scp "${SSH_CONFIG_ARGS[@]}" -i "${ARYAOS_DEV_PI_SSH_KEY}" -o IdentitiesOnly=yes "${KNOWN_HOSTS_ARGS[@]}")
 	SSH_METHOD="explicit key ${ARYAOS_DEV_PI_SSH_KEY}"
 elif [[ -r "${DEV_KEY}" && -z "${ARYAOS_DEV_PI_SKIP_KEY:-}" ]]; then
-	SSH=(ssh -i "${DEV_KEY}" -o IdentitiesOnly=yes -o BatchMode=yes -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
-	SCP=(scp -i "${DEV_KEY}" -o IdentitiesOnly=yes "${KNOWN_HOSTS_ARGS[@]}")
+	SSH=(ssh "${SSH_CONFIG_ARGS[@]}" -i "${DEV_KEY}" -o IdentitiesOnly=yes -o BatchMode=yes -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
+	SCP=(scp "${SSH_CONFIG_ARGS[@]}" -i "${DEV_KEY}" -o IdentitiesOnly=yes "${KNOWN_HOSTS_ARGS[@]}")
 	SSH_METHOD="repo lab key ${DEV_KEY}"
 	if ! "${SSH[@]}" "${PI}" true 2>/dev/null; then
-		SSH=(ssh -o BatchMode=yes -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
-		SCP=(scp "${KNOWN_HOSTS_ARGS[@]}")
+		SSH=(ssh "${SSH_CONFIG_ARGS[@]}" -o BatchMode=yes -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
+		SCP=(scp "${SSH_CONFIG_ARGS[@]}" "${KNOWN_HOSTS_ARGS[@]}")
 		SSH_METHOD="ssh defaults"
 	fi
 fi
@@ -56,16 +60,16 @@ fi
 if ! "${SSH[@]}" "${PI}" true 2>/dev/null; then
 	if [[ -n "${ARYAOS_DEV_PI_PASSWORD:-}" ]] && command -v sshpass >/dev/null; then
 		export SSHPASS="${ARYAOS_DEV_PI_PASSWORD}"
-		SSH=(sshpass -e ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
-		SCP=(sshpass -e scp -o PreferredAuthentications=password -o PubkeyAuthentication=no "${KNOWN_HOSTS_ARGS[@]}")
+		SSH=(sshpass -e ssh "${SSH_CONFIG_ARGS[@]}" -o PreferredAuthentications=password -o PubkeyAuthentication=no -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
+		SCP=(sshpass -e scp "${SSH_CONFIG_ARGS[@]}" -o PreferredAuthentications=password -o PubkeyAuthentication=no "${KNOWN_HOSTS_ARGS[@]}")
 		SSH_METHOD="password"
 	elif command -v sshpass >/dev/null; then
 		# shellcheck disable=SC1091
 		[[ -f scripts/.dev-pi-creds.local ]] && . scripts/.dev-pi-creds.local
 		if [[ -n "${ARYAOS_DEV_PI_PASSWORD:-}" ]]; then
 			export SSHPASS="${ARYAOS_DEV_PI_PASSWORD}"
-			SSH=(sshpass -e ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
-			SCP=(sshpass -e scp -o PreferredAuthentications=password -o PubkeyAuthentication=no "${KNOWN_HOSTS_ARGS[@]}")
+			SSH=(sshpass -e ssh "${SSH_CONFIG_ARGS[@]}" -o PreferredAuthentications=password -o PubkeyAuthentication=no -o ConnectTimeout=12 "${KNOWN_HOSTS_ARGS[@]}")
+			SCP=(sshpass -e scp "${SSH_CONFIG_ARGS[@]}" -o PreferredAuthentications=password -o PubkeyAuthentication=no "${KNOWN_HOSTS_ARGS[@]}")
 			SSH_METHOD="password"
 		fi
 	fi
